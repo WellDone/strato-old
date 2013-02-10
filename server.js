63ebc9e7694ec63601db250460dbf2f92f2c9c8b
Austin McGee <austinwm@gmail.com>
2013-02-09 16:34:30 -0800

    Initial server commit - Node.js with Express.js, serving up static files.

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require( 'express' );
+
+var app = express();
+
+app.get( '/', function( req, res ) {
+	res.sendfile( __dirname + "/resources/html/index.html" );
+});
+app.use( '/resources', express.static( __dirname + "/resources" ) );
+
+var port = 3000;
+app.listen( port );
+console.log( "Listening on port" + port + "." );
