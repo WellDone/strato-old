66c013fe294852e3750a73d3dea23fbe679249ad
Austin McGee <austinwm@gmail.com>
2013-07-21 14:32:53 -0700

    Fix the database seed script.

--- db/seed.js
+++ db/seed.js
@@ -38,19 +38,12 @@ function dropAllFromTable( table) {
 }
 
 setTimeout( function() {
-if ( clean ) {
-  if ( process.argv.length != 3 ) {
-    console.log( "USAGE: node seed.js --clean" );
-    console.log( "       node seed.js [seedFile]" );
-    process.exit(1);
-  }
   console.log( "Cleaning the DB.  Goodbye." );
   dropAllFromTable( "malformed_reports" );
   dropAllFromTable( "aggregate_reports" );
   dropAllFromTable( "monitors" );
   dropAllFromTable( "sites" );
-  return;
-} else {
+
   var seedFile;
   if ( process.argv.length == 2 ) {
     seedFile = './data/test.seed';
@@ -62,25 +55,24 @@ if ( clean ) {
     process.exit(1);
   }
   var data = require( seedFile );
-
-  console.log( "Seeding database.  " + data.length + " site." );
+  console.log( "Seeding database.  " + data.length + " sites." );
   for ( var i=0; i<data.length; ++i ) {
-    q("INSERT INTO sites( name, country ) VALUES ('" + data[i].name + "','" + data[i].country + "')" )
-    for ( var j=0; j<data[i].monitors.length; ++j ) {
-      var m = data[i].monitors[j];
-      var av = 650 / 8 + Math.random() * 20 - 10;
-      av *= m._functionality;
-      if ( !m.breakdown ) {
-        m.breakdown = dates.length;
-      }
-      q( "INSERT INTO monitors( name, location, siteid ) VALUES ('" + m.name + "','(" + m.lat + "," + m.lng + ")'," + i + ")" )
-      .on( 'end', function( j, av, breakdown ) {
-        for ( var k=0; k<dates.length; ++k ) {
-          var v = (k>=breakdown)?4:av + Math.random() * 30 - 15;
-          q( "INSERT INTO reports( timestamp, eventcount, monitorid ) VALUES ('" + dates[k] + "'," + v + "," + j + ")");
+    q("INSERT INTO sites( id, name, country ) VALUES (" + i + ",'" + data[i].name + "','" + data[i].country + "')" ).on( 'end', function(i) {
+      for ( var j=0; j<data[i].monitors.length; ++j ) {
+        var m = data[i].monitors[j];
+        var av = 650 / 8 + Math.random() * 20 - 10;
+        av *= m._functionality;
+        if ( !m.breakdown ) {
+          m.breakdown = dates.length;
         }
-      }.bind( null, j, av, m.breakdown ) );
-    }
+        q( "INSERT INTO monitors( id, GSMID, name, location, siteid ) VALUES (" + j + "," + j + ",'" + m.name + "','(" + m.lat + "," + m.lng + ")'," + i + ")" )
+        .on( 'end', function( j, av, breakdown ) {
+          for ( var k=0; k<dates.length; ++k ) {
+            var v = (k>=breakdown)?4:av + Math.random() * 30 - 15;
+            q( "INSERT INTO aggregate_reports( timestamp, eventcount, monitorid ) VALUES ('" + dates[k] + "'," + v + "," + j + ")");
+          }
+        }.bind( null, j, av, m.breakdown ) );
+      }
+    }.bind(null, i) )
   }
-}
 }, 500 );
