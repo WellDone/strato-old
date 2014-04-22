862ebd56c4b7548b405ae2cdc12b5bc25bf4aacf
Chris Loer <chris.loer@gmail.com>
2014-04-22 11:19:55 -0500

    Generic text-based filtering for management pages.

--- app/static/js/app/manage-page.js
+++ app/static/js/app/manage-page.js
@@ -49,105 +49,129 @@ define( [ 'jquery',
  		}
  		return out;
  	}
- 	Renderer.prototype.render = function() {
- 		var self = this;
+ 	Renderer.prototype.filterData = function( data, filter ) {
+ 		if ( !filter || filter.length == 0 )
+ 			return data;
+ 		return data.filter( function( flatObj ) {
+ 			for ( var index in flatObj.columns )
+	 			if ( flatObj.columns[index].toString().indexOf(filter) !== -1 )
+		 			return true;
+
+		 	return false;
+ 		});
+ 	}
+ 	Renderer.prototype.renderData = function( data, self )
+ 	{
  		var dom = $(self.dom);
- 		$.getJSON( this.url, function(data) {
- 			var obj = {
- 				name: self.name,
- 				columns: self.columns,
- 				data: self.flattenData( data )
- 			}
- 			dom.html( template( obj ) );
- 			dom.find('#deleteModal').modal( { show: false } );
+ 		var filterText = dom.find('#filter-input').val();
+		var obj = {
+				name: self.name,
+				columns: self.columns,
+				data: self.filterData( self.flattenData( data ), filterText ),
+				filter: filterText
+			}
+			dom.html( template( obj ) );
+			dom.find('#filter-input').focus();
+			if ( filterText && filterText.length > 0 )
+				dom.find('#filter-input').val( filterText );
 
- 			dom.find( 'tr.linkRow').click( function (e) {
-				var event = e || window.event;
-				page( "/manage/" + self.name.raw + "/" + $(this).attr('data-id') );
+			dom.find('#deleteModal').modal( { show: false } );
+
+			dom.find( 'tr.linkRow').click( function (e) {
+			var event = e || window.event;
+			page( "/manage/" + self.name.raw + "/" + $(this).attr('data-id') );
+			e.stopPropagation();
+		})
+
+		dom.find('#filter-input').on( 'keyup', function() { Renderer.prototype.renderData( data, self ) } );
+
+			if ( self.permissions.create )
+			{
+				dom.find( '#create-resource-button').click( function(e) {
+					e = e || window.event;
+				e.preventDefault();
 				e.stopPropagation();
+				var data = {};
+				var done = false;
+				dom.find( '#create-resource-row > td > input').each( function( i ) {
+					var el = $(this);
+					var name = el.attr('data-name');
+					var val = el.val();
+					if ( !val && !done )
+					{
+						done = true;
+						alert( "Invalid value specified for " + name + "." );
+					}
+					data[name] = val;
+				})
+				if ( done )
+					return;
+				$.ajax({
+					url: self.url,
+					type: 'POST',
+					data: data,
+					complete: function(result) {
+						// TODO: Display errors
+						self.render();
+					}
+				});
 			})
+			dom.find( '#plus-button').click( function (e) {
+				dom.find( '#create-resource-row').removeClass( 'hidden' );
+				dom.find( '#plus-button-row').addClass( 'hidden' );
+			})
+		}
+		else
+		{
+			dom.find( '#plus-button-row' ).addClass( 'hidden' );
+		}
 
- 			if ( self.permissions.create )
- 			{
- 				dom.find( '#create-resource-button').click( function(e) {
- 					e = e || window.event;
-					e.preventDefault();
-					e.stopPropagation();
-					var data = {};
-					var done = false;
-					dom.find( '#create-resource-row > td > input').each( function( i ) {
-						var el = $(this);
-						var name = el.attr('data-name');
-						var val = el.val();
-						if ( !val && !done )
-						{
-							done = true;
-							alert( "Invalid value specified for " + name + "." );
-						}
-						data[name] = val;
-					})
-					if ( done )
-						return;
+		if ( self.permissions.del )
+		{
+			dom.find( 'a.deleteResource').click( function(e) {
+				e = e || window.event;
+				var id = $(this).parent().parent().attr('data-id');
+				var delFunc = function() {
 					$.ajax({
-						url: self.url,
-						type: 'POST',
-						data: data,
+						url: self.url + '/' + id,
+						type: 'DELETE',
 						complete: function(result) {
-							// TODO: Display errors
+							dom.find( '#delete-modal').modal('hide');
+							//TODO: These shouldn't be needed, but they are.
+							$('body').removeClass('modal-open');
+							$('.modal-backdrop').remove();
+
 							self.render();
 						}
 					});
-				})
-				dom.find( '#plus-button').click( function (e) {
-					dom.find( '#create-resource-row').removeClass( 'hidden' );
-					dom.find( '#plus-button-row').addClass( 'hidden' );
-				})
-			}
-			else
-			{
-				dom.find( '#plus-button-row' ).addClass( 'hidden' );
-			}
+					dom.find( '#actually-delete-button').off( 'click', delFunc );
+				}
+				dom.find( '#deleteModal').modal('show');
+				dom.find( '#actually-delete-button').on( 'click', delFunc );
+				e.stopPropagation();
+			});
+		}
+		else
+		{
+			dom.find( '.deleteResource' ).addClass( 'hidden' );
+		}
 
-			if ( self.permissions.del )
-			{
-				dom.find( 'a.deleteResource').click( function(e) {
-					e = e || window.event;
-					var id = $(this).parent().parent().attr('data-id');
-					var delFunc = function() {
-						$.ajax({
-							url: self.url + '/' + id,
-							type: 'DELETE',
-							complete: function(result) {
-								dom.find( '#delete-modal').modal('hide');
-								//TODO: These shouldn't be needed, but they are.
-								$('body').removeClass('modal-open');
-								$('.modal-backdrop').remove();
+		if ( !self.permissions.edit )
+		{
+			dom.find( '.editResource' ).addClass( 'hidden' );
+		}
 
-								self.render();
-							}
-						});
-						dom.find( '#actually-delete-button').off( 'click', delFunc );
-					}
-					dom.find( '#deleteModal').modal('show');
-					dom.find( '#actually-delete-button').on( 'click', delFunc );
-					e.stopPropagation();
-				});
-			}
-			else
-			{
-				dom.find( '.deleteResource' ).addClass( 'hidden' );
-			}
-
-			if ( !self.permissions.edit )
-			{
-				dom.find( '.editResource' ).addClass( 'hidden' );
-			}
-
-			if ( !self.permissions.edit && !self.permissions.del )
-			{
-				dom.find( '.resource-manager' ).addClass( 'hidden' );
-			}
-	 	} );
+		if ( !self.permissions.edit && !self.permissions.del )
+		{
+			dom.find( '.resource-manager' ).addClass( 'hidden' );
+		}
+	
+ 	}
+ 	Renderer.prototype.render = function() {
+ 		 var self = this;
+ 		$.getJSON( this.url, function(data) {
+ 			Renderer.prototype.renderData( data, self );
+ 	 	} ); 			
  	}
 
  	return Renderer;
