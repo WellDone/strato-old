fb7b60ba98eb9e22a575d95f89415f489d9b9aea
Chris Loer <chris.loer@gmail.com>
2014-04-30 21:36:20 -0500

    Starting to use backbone.js in manage-page, avoiding re-render of whole page when search criteria change.

--- app/static/js/app/manage-page.js
+++ app/static/js/app/manage-page.js
@@ -1,178 +1,181 @@
 define( [ 'jquery',
-	        'page',
-          'hbars!views/manage-page' ],
- function( $, page, template ) {
- 	var Renderer = function( dom, args )
- 	{
- 		this.dom = dom;
- 		this.name = args.name;
- 		if ( !this.name.raw )
- 			throw new Error( "A name must be specified to the manage-page renderer." )
- 		this.url = '/api/v0/' + this.name.raw
- 		this.columns = [];
- 		this.permissions = {
- 			readonly: args.readonly,
- 			create: (!args.readonly && args.create),
- 			edit: !args.readonly,
- 			del: !args.readonly
- 		}
- 		for ( var c in args.columns )
- 		{
- 			var column = { raw: c, pretty: args.columns[c] }
- 			if ( args.create[c] )
- 			{
- 				column.create = { placeholder: args.create[c] }
- 			}
- 			this.columns.push( column );
- 		}
- 	};
- 	Renderer.prototype.flattenData = function( data ) {
- 		var out = [];
- 		for ( var d in data )
- 		{
- 			var flatObj = { id: data[d].id, columns: [] };
-	 		for ( var i = 0; i < this.columns.length; ++i )
-	 		{
-	 			var item = data[d][this.columns[i].raw];
-	 			if ( !item )
-	 				item = ""
-	 			if ( $.isArray( item ) )
-	 			{
-	 				item = {
-	 					isRef: true,
-	 					refs: item
-	 				}
-	 			}
-	 			flatObj.columns.push( item );
-	 		}
-	 		out.push( flatObj );
- 		}
- 		return out;
- 	}
- 	Renderer.prototype.filterData = function( data, filter ) {
- 		if ( !filter || filter.length == 0 )
- 			return data;
- 		return data.filter( function( flatObj ) {
- 			for ( var index in flatObj.columns )
-	 			if ( flatObj.columns[index].toString().indexOf(filter) !== -1 )
-		 			return true;
+	'page',
+	'backbone',
+	'hbars!views/manage-page',
+	'hbars!views/manage/management-item' ],
+	function( $, page, backbone, pageTemplate, itemTemplate ) {
+		var Renderer = function( dom, args )
+		{
+			this.dom = dom;
+			this.name = args.name;
+			if ( !this.name.raw )
+				throw new Error( "A name must be specified to the manage-page renderer." );
+			this.columns = [];
+			this.items = new Backbone.Collection();
+			this.items.url = '/api/v0/' + this.name.raw;
+			this.items.fetch();
+			this.permissions = {
+				readonly: args.readonly,
+				create: (!args.readonly && args.create),
+				edit: !args.readonly,
+				del: !args.readonly
+			};
+			for ( var c in args.columns )
+			{
+				var column = { raw: c, pretty: args.columns[c] };
+				if ( args.create[c] )
+				{
+					column.create = { placeholder: args.create[c] };
+				}
+				this.columns.push( column );
+			}
+			var ManagementListItemView = Backbone.View.extend({
+				tagName: 'tr',
+				className: 'linkRow',
 
-		 	return false;
- 		});
- 	}
- 	Renderer.prototype.renderData = function( data, self )
- 	{
- 		var dom = $(self.dom);
- 		var filterText = dom.find('#filter-input').val();
-		var obj = {
+				render: function() {
+					var $el = $(this.el), self = this;
+					$el.attr("data-id", self.model.attributes['id']);
+					$el.click( function (e) {
+						var event = e || window.event;
+						page( "/manage/" + args.name.raw + "/" + self.model.attributes['id'] );
+						e.stopPropagation();
+					});
+					$el.html( itemTemplate( { columns: _.omit( this.model.attributes, "id" ) } ) );
+
+					return this;
+				},
+			});
+			renderer = this;
+			this.ManagementListView = Backbone.View.extend({
+				initialize: function() {
+					this.collection.on('add', this.render, this);
+				},
+
+				render: function() {
+					var $el = $("#managementItemTable"), self = this;
+
+					$el.empty();
+					var filterText = $('#filter-input').val();
+					this.collection.each(function(data) {
+						var matchedFilter = _.find( _.values( _.omit( data.attributes, "id" ) ), function( value ) {
+							return !filterText || filterText.length == 0 || value.toString().toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+						} );
+						if ( matchedFilter )
+						{
+							var item, sidebarItem;
+							item = new ManagementListItemView({ model: data });
+							$el.append(item.render().el);
+						}
+					});
+
+					if ( renderer.permissions.del )
+					{
+						$( 'a.deleteResource').click( function(e) {
+							e = e || window.event;
+							var id = $(this).parent().parent().attr('data-id');
+							var delFunc = function() {
+								$.ajax({
+									url: renderer.items.url + '/' + id,
+									type: 'DELETE',
+									complete: function(result) {
+										$( '#delete-modal').modal('hide');
+										//TODO: These shouldn't be needed, but they are.
+										$('body').removeClass('modal-open');
+										$('.modal-backdrop').remove();
+
+										renderer.render();
+									}
+								});
+								$( '#actually-delete-button').off( 'click', delFunc );
+							};
+							$( '#deleteModal').modal('show');
+							$( '#actually-delete-button').on( 'click', delFunc );
+							e.stopPropagation();
+						});
+					}
+					else
+					{
+						$( '.deleteResource' ).addClass( 'hidden' );
+					}
+
+					if ( !renderer.permissions.edit )
+					{
+						$( '.editResource' ).addClass( 'hidden' );
+					}
+
+					if ( !renderer.permissions.edit && !renderer.permissions.del )
+					{
+						$( '.resource-manager' ).addClass( 'hidden' );
+					}
+
+					return this;
+				}
+			});
+			this.itemList = new this.ManagementListView({ collection: this.items });
+		};
+		Renderer.prototype.renderData = function( self )
+		{
+			var dom = $(self.dom);
+			var filterText = dom.find('#filter-input').val();
+			var obj = {
 				name: self.name,
 				columns: self.columns,
-				data: self.filterData( self.flattenData( data ), filterText ),
 				filter: filterText
-			}
-			dom.html( template( obj ) );
-			dom.find('#filter-input').focus();
-			if ( filterText && filterText.length > 0 )
-				dom.find('#filter-input').val( filterText );
+			};
+			dom.html( pageTemplate( obj ) );
 
 			dom.find('#deleteModal').modal( { show: false } );
 
-			dom.find( 'tr.linkRow').click( function (e) {
-			var event = e || window.event;
-			page( "/manage/" + self.name.raw + "/" + $(this).attr('data-id') );
-			e.stopPropagation();
-		})
-
-		dom.find('#filter-input').on( 'keyup', function() { Renderer.prototype.renderData( data, self ) } );
+			self.itemList.render();
+			dom.find('#filter-input').on( 'keyup', function() { self.itemList.render(); } );
 
 			if ( self.permissions.create )
 			{
 				dom.find( '#create-resource-button').click( function(e) {
 					e = e || window.event;
-				e.preventDefault();
-				e.stopPropagation();
-				var data = {};
-				var done = false;
-				dom.find( '#create-resource-row > td > input').each( function( i ) {
-					var el = $(this);
-					var name = el.attr('data-name');
-					var val = el.val();
-					if ( !val && !done )
-					{
-						done = true;
-						alert( "Invalid value specified for " + name + "." );
-					}
-					data[name] = val;
-				})
-				if ( done )
-					return;
-				$.ajax({
-					url: self.url,
-					type: 'POST',
-					data: data,
-					complete: function(result) {
-						// TODO: Display errors
-						self.render();
-					}
-				});
-			})
-			dom.find( '#plus-button').click( function (e) {
-				dom.find( '#create-resource-row').removeClass( 'hidden' );
-				dom.find( '#plus-button-row').addClass( 'hidden' );
-			})
-		}
-		else
-		{
-			dom.find( '#plus-button-row' ).addClass( 'hidden' );
-		}
-
-		if ( self.permissions.del )
-		{
-			dom.find( 'a.deleteResource').click( function(e) {
-				e = e || window.event;
-				var id = $(this).parent().parent().attr('data-id');
-				var delFunc = function() {
+					e.preventDefault();
+					e.stopPropagation();
+					var data = {};
+					var done = false;
+					dom.find( '#create-resource-row > td > input').each( function( i ) {
+						var el = $(this);
+						var name = el.attr('data-name');
+						var val = el.val();
+						if ( !val && !done )
+						{
+							done = true;
+							alert( "Invalid value specified for " + name + "." );
+						}
+						data[name] = val;
+					});
+					if ( done )
+						return;
 					$.ajax({
-						url: self.url + '/' + id,
-						type: 'DELETE',
+						url: self.items.url,
+						type: 'POST',
+						data: data,
 						complete: function(result) {
-							dom.find( '#delete-modal').modal('hide');
-							//TODO: These shouldn't be needed, but they are.
-							$('body').removeClass('modal-open');
-							$('.modal-backdrop').remove();
-
+							// TODO: Display errors
 							self.render();
+							self.items.fetch();
 						}
 					});
-					dom.find( '#actually-delete-button').off( 'click', delFunc );
-				}
-				dom.find( '#deleteModal').modal('show');
-				dom.find( '#actually-delete-button').on( 'click', delFunc );
-				e.stopPropagation();
-			});
-		}
-		else
-		{
-			dom.find( '.deleteResource' ).addClass( 'hidden' );
-		}
+				});
+				dom.find( '#plus-button').click( function (e) {
+					dom.find( '#create-resource-row').removeClass( 'hidden' );
+					dom.find( '#plus-button-row').addClass( 'hidden' );
+				});
+			}
+			else
+			{
+				dom.find( '#plus-button-row' ).addClass( 'hidden' );
+			}
+		};
+		Renderer.prototype.render = function() {
+			var self = this;
+			Renderer.prototype.renderData( self );
+		};
 
-		if ( !self.permissions.edit )
-		{
-			dom.find( '.editResource' ).addClass( 'hidden' );
-		}
-
-		if ( !self.permissions.edit && !self.permissions.del )
-		{
-			dom.find( '.resource-manager' ).addClass( 'hidden' );
-		}
-	
- 	}
- 	Renderer.prototype.render = function() {
- 		 var self = this;
- 		$.getJSON( this.url, function(data) {
- 			Renderer.prototype.renderData( data, self );
- 	 	} ); 			
- 	}
-
- 	return Renderer;
- })
+		return Renderer;
+	});
