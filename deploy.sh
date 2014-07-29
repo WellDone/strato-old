d90096224f2ea8bc6398127a987c8224f6ccf19d
Austin McGee <austinwm@gmail.com>
2014-07-29 21:16:41 +0300

    Pull deployment files from github rather than whatever is on the local machine.

--- deploy.sh
+++ deploy.sh
@@ -59,29 +59,64 @@ elif [ "$1" == "local" ]; then
 else
 	TARGET_MACHINE="$1"
 	SSH_CMD="ssh $TARGET_MACHINE"
-	$SSH_CMD "sudo mkdir -p /welldone/tmp"
 
-	tar -cz ./app | $SSH_CMD "sudo tar -xzC /welldone/tmp"
-	tar -cz ./config | $SSH_CMD "sudo tar -xzC /welldone/tmp"
+	echo "Downloading Git..."
+	$SSH_CMD "sudo apt-get install -y git" 2>1 >> deploy.log
+	if [ $? -ne 0 ]; then
+		echo "Failed!"
+		exit 1
+	fi
+	echo "Downloading Strato from GitHub..."
+	$SSH_CMD "sudo mkdir -p /welldone_tmp
+	          sudo chmod a+rw /welldone_tmp
+	          cd /welldone_tmp
+	          git clone https://github.com/welldone/strato.git
+	          cd strato
+	          git checkout master
+	          git pull
+	          cd app
+	          npm update" 2>1 >> deploy.log
+	if [ $? -ne 0 ]; then
+		echo "Failed!"
+		exit 1
+	fi
+	echo "Backing up existing files..."
+	$SSH_CMD "sudo mkdir -p /welldone
+	          rm -rf /welldone_backup
+	          sudo mv -f /welldone /welldone_backup" 2>1 >> deploy.log
+	if [ $? -ne 0 ]; then
+		echo "Failed!"
+		exit 1
+	fi
+	echo "Hotswapping..."
+	$SSH_CMD "sudo mv -f /welldone_tmp/strato /welldone
+            sudo rm -rf /welldone_tmp /welldone_backup" 2>1 >> deploy.log
+  if [ $? -ne 0 ]; then
+  	echo "Failed!  Restoring..."
+		$SSH_CMD "sudo mv -f /welldone_backup /welldone" 2>1 >> deploy.log
+		exit 1
+	fi
 
-	echo "Synchronizing ./app and ./config..."
-	$SSH_CMD "cd /welldone
-	          rm -rf ./backup
-	          sudo mkdir backup
-	          if [ -e ./app ]; then sudo mv -f ./app ./backup/app; fi
-            if [ -e ./config ]; then sudo mv -f ./config ./backup/config; fi
-            sudo mv -f ./tmp/* ./
-            sudo rm -rf ./tmp ./backup"
 fi
 
 if [ -n "$PROVISION" ]; then
 	echo "Provisioning server"
-	$SSH_CMD "sudo bash -c 'export APP_CONTEXT=$APP_CONTEXT; /welldone/config/provision.sh'"
+	$SSH_CMD "sudo bash -c 'export APP_CONTEXT=$APP_CONTEXT; /welldone/config/provision.sh'" 2>1 >> deploy.log
 fi
 
-$SSH_CMD "cd /welldone/app
+echo "Restarting the process..."
+$SSH_CMD "set -e
+          cd /welldone/app
+          sudo service nginx restart
           sudo bash ./control/stop.sh
           chmod +x ./control/initdb.sh
           sudo -u postgres bash -c './control/initdb.sh $CLEAN'
           chmod +x ./control/start.sh
-          sudo -u application bash -c 'export DATABASE_URL=tcp://dbadmin:GikmnmJKDOB3@localhost:5432/welldone; ./control/start.sh'"
+          sudo -u application bash -c 'export DATABASE_URL=tcp://dbadmin:GikmnmJKDOB3@localhost:5432/welldone; ./control/start.sh'" 2>1 >> deploy.log
+if [ $? -ne 0 ]; then
+	echo "Failed!  Recovering..."
+	$SSH_CMD "sudo mv -f /welldone_backup /welldone" 2>1 >> deploy.log
+	exit 1
+fi
+
+echo "SUCCESS!"
